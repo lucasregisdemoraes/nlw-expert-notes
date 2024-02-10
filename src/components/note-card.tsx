4593330cbc1dece6b31ecc0160ad066165580473
lucasregisdemoraes <lucasgrm03@gmail.com>
2024-02-10 15:57:33 -0300

    finish class 1

---
+++ src/components/note-card.tsx
@@ -0,0 +1,14 @@
+export function NoteCard() {
+    return (
+        <button type="button" className="rounded-md text-left bg-slate-800 p-5 space-y-3 relative overflow-hidden outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
+            <span className="text-sm font-medium text-slate-200">
+                Há 2 dias
+            </span>
+            <p className="text-sm leading-6 text-slate-400">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius consequatur quos aut facere quas at ducimus quae repellat est delectus, nihil cum ea dicta commodi a vel impedit reprehenderit labore.
+            </p>
+
+            <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none" />
+        </button>
+    )
+}
