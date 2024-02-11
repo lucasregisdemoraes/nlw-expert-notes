a85f3e0b9593d1650c23a8b0d25cbbb3f07e6e62
lucasregisdemoraes <lucasgrm03@gmail.com>
2024-02-11 17:13:31 -0300

    finish class 3

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,61 @@
+import { ChangeEvent, useState } from 'react'
 import logo from './assets/logo-nlw-expert.svg'
 import { NewNoteCard } from './components/new-note-card'
 import { NoteCard } from './components/note-card'
 
+interface Note {
+  id: string,
+  date: Date,
+  content: string
+}
+
 export function App() {
+  const [search, setSearch] = useState('')
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorage = localStorage.getItem('notes')
+
+    if (notesOnStorage) {
+      return JSON.parse(notesOnStorage)
+    }
+    return []
+  })
+
+  function onNoteCreated(content: string) {
+    const newNote = {
+      id: crypto.randomUUID(),
+      date: new Date(),
+      content,
+    }
+
+    const notesArray = [newNote, ...notes]
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function onNoteDeleted(id: string) {
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
+  function handleSearch(event: ChangeEvent<HTMLInputElement>) {
+    const query = event.target.value
+
+    setSearch(query)
+  }
+
+  const filteredNotes = search !== ""
+    ? notes.filter(note => note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase()))
+    : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5">
       <img src={logo} alt="NLW Expert" />
 
       <form className="w-full">
@@ -13,17 +63,15 @@ export function App() {
           type="text"
           placeholder='Busque em suas notas...'
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
         />
       </form>
       <div className="h-px bg-slate-700" />
 
-      <div className="grid gap-6 grid-cols-3 auto-rows-[250px]">
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px]">
 
-        <NewNoteCard />
-        <NoteCard note={{
-          date: new Date,
-          content: "sadfasdf"
-        }} />
+        <NewNoteCard onNoteCreated={onNoteCreated} />
+        {filteredNotes.map(note => <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted} />)}
       </div>
     </div>
   )
--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -5,12 +5,14 @@ import { X } from 'lucide-react'
 
 interface NoteCardProps {
     note: {
+        id: string
         date: Date
         content: string
     }
+    onNoteDeleted: (id: string) => void
 }
 
-export function NoteCard({ note }: NoteCardProps) {
+export function NoteCard({ note, onNoteDeleted }: NoteCardProps) {
     return (
         <Dialog.Root>
             <Dialog.Trigger type="button" className="rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 relative overflow-hidden outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-lime-400">
@@ -26,7 +28,7 @@ export function NoteCard({ note }: NoteCardProps) {
 
             <Dialog.Portal>
                 <Dialog.Overlay className='inset-0 fixed bg-black/60' />
-                <Dialog.Content className='fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] h-[60vh] w-full bg-slate-700 rounded-md flex flex-col outline-none'>
+                <Dialog.Content className='fixed overflow-hidden inset-0 md:inset-auto md:left-1/2 md:top-1/2 md:-translate-x-1/2 md:-translate-y-1/2 md:max-w-[640px] md:h-[60vh] w-full bg-slate-700 md:rounded-md flex flex-col outline-none'>
                     <Dialog.Close className='absolute right-0 top-0 p-1.5 bg-slate-800 text-slate-400 hover:text-slate-100 focus-visible:text-slate-100 outline-none'>
                         <X className='size-5' />
                     </Dialog.Close>
@@ -41,6 +43,7 @@ export function NoteCard({ note }: NoteCardProps) {
 
                     <button
                         type="button"
+                        onClick={() => onNoteDeleted(note.id)}
                         className='w-full bg-slate-800 py-4 text-center text-sm text-slate-300 outline-none font-medium group'
                     >
                         Deseja <span className="text-red-400 group-hover:underline group-focus-visible:underline">apagar essa nota</span>?
